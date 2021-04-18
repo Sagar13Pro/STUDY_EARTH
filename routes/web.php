3cbf6d082a67ccdfe16d838e3d705ed1845d4c4f
Sagar13Pro <76509231+Sagar13Pro@users.noreply.github.com>
2021-04-18 09:30:20 +0530

    Index Page
    mainConrtoller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\mainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//==================================VIEW=========================================
+Route::get('/', [mainController::class, 'Index'])->name('index.view');
+//===============================================================================
