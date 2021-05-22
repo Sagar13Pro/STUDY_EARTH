3bf77f07e816826d4e73c03038666d26028abc60
Sagar13Pro <76509231+Sagar13Pro@users.noreply.github.com>
2021-05-22 10:00:07 +0530

    Table course_material created

---
+++ database/migrations/2021_05_22_095805_create_course_materials_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_materials', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_detail_id')->constrained('course_details');
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('pdf_path');
+            $table->double('display_order');
+            $table->enum('isDeleted', ['No', 'Yes']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_materials');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,6 @@ use App\Http\Controllers\mainController;
 use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 //==================================PROJECTS ROUTES=============================================
 Route::get('/', [mainController::class, 'IndexView'])->name('index.view');
